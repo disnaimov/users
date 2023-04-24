ad6d5bbd5b6f16afe07378e1865628866991075d
disna <@dsn>
2023-04-24 11:48:55 +0400

    Убрал роль в миграции

--- src/main/resources/create_users.sql
+++ src/main/resources/create_users.sql
@@ -3,7 +3,6 @@ create table if not exists users
     id          uuid         not null,
     first_name  varchar(100) not null,
     last_name   varchar(100) not null,
-    role        varchar,
     UNIQUE      (id),
     PRIMARY KEY (id)
     );
