b89c234255e9f7b060a9253db1eb0a7596208dfd
disna <@dsn>
2023-05-03 10:34:58 +0400

    рабочий для юзера и мессенджа

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,21 +1,9 @@
 package com.example.demo;
 
-import com.example.demo.config.MapperUtil;
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
-
-/*//@ComponentScan(basePackages={"<com.example>"})
-//@EnableJpaRepositories(basePackages="<com.example.demo.dao.UserRepository>")
-@EnableTransactionManagement
-@EntityScan(basePackages="<entities>")*/
 @SpringBootApplication
 public class DemoApplication {
 
